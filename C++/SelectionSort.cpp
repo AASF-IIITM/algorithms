225ad877c2869ccf0cd4d6e0c8b42ca7a2811b52
savi-1311 <shambhavishandilya01@gmail.com>
2020-04-01 13:43:11 +0530

    Added Selection sort

---
+++ C++/SelectionSort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void selectionSort(int arr[],int n)
+{
+  for(int i=0;i<n-1;i++) // checking each and every element as boundary element
+  {
+  	for(int j=i+1;j<n;j++) //travering the sub-array for any smaller element
+  	{
+  		if(arr[j]<arr[i]) //swapping the elemnts if the subarray element is smaller than boundary element
+  		{
+  			int temp = arr[i];
+  			arr[i] = arr[j];
+  			arr [j] = temp;
+		}
+	}
+  }
+  cout << "\nThe sorted array is-\n"; //printing the sorted array
+  	for(int i=0;i<n;i++)
+	{
+		cout << arr[i] << "\n";
+	}	
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the no. of Elements-\n";
+	cin >> n;
+	int arr[n];
+	for(int i=0;i<n;i++) //Entering elements in the array
+	{
+		cout << "Enter the elements-\n";
+		cin >> arr[i];
+	}
+	selectionSort(arr,n);
+	
+}
