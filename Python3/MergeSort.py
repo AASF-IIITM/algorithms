d5989099ef63f8a6c9ee778186f9987439d2d947
img037 <72347317+img037@users.noreply.github.com>
2020-10-04 20:18:58 +0530

    Update MergeSort.py

--- Python3/MergeSort.py
+++ Python3/MergeSort.py
@@ -1,16 +1,15 @@
-# Python program for implementation of MergeSort 
 def mergeSort(arr): 
     if len(arr) >1: 
-        mid = len(arr)//2 # Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
+        mid = len(arr)//2  
+        L = arr[:mid]  
+        R = arr[mid:] 
   
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
+        mergeSort(L) 
+        mergeSort(R) 
   
         i = j = k = 0
           
-        # Copy data to temp arrays L[] and R[] 
+    
         while i < len(L) and j < len(R): 
             if L[i] < R[j]: 
                 arr[k] = L[i] 
@@ -20,7 +19,7 @@ def mergeSort(arr):
                 j+= 1
             k+= 1
           
-        # Checking if any element was left 
+       
         while i < len(L): 
             arr[k] = L[i] 
             i+= 1
@@ -31,13 +30,13 @@ def mergeSort(arr):
             j+= 1
             k+= 1
   
-# Code to print the list 
+
 def printList(arr): 
     for i in range(len(arr)):         
         print(arr[i], end =" ") 
     print() 
   
-# driver code to test the above code 
+
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end ="\n")  
