aea65dd759eac28c43fc7a4107061866e70eaf36
c98s <55761694+c98s@users.noreply.github.com>
2021-10-05 11:19:18 +0530

    Create heap_sort.go

---
+++ Go/heap_sort.go
@@ -0,0 +1,43 @@
+package sort
+
+type maxHeap struct {
+	slice    []int
+	heapSize int
+}
+
+func buildMaxHeap(slice []int) maxHeap {
+	h := maxHeap{slice: slice, heapSize: len(slice)}
+	for i := len(slice) / 2; i >= 0; i-- {
+		h.MaxHeapify(i)
+	}
+	return h
+}
+
+func (h maxHeap) MaxHeapify(i int) {
+	l, r := 2*i+1, 2*i+2
+	max := i
+
+	if l < h.size() && h.slice[l] > h.slice[max] {
+		max = l
+	}
+	if r < h.size() && h.slice[r] > h.slice[max] {
+		max = r
+	}
+	if max != i {
+		h.slice[i], h.slice[max] = h.slice[max], h.slice[i]
+		h.MaxHeapify(max)
+	}
+}
+
+func (h maxHeap) size() int { return h.heapSize } // ???
+
+func HeapSort(slice []int) []int {
+	h := buildMaxHeap(slice)
+	for i := len(h.slice) - 1; i >= 1; i-- {
+		h.slice[0], h.slice[i] = h.slice[i], h.slice[0]
+		h.heapSize--
+		h.MaxHeapify(0)
+		
+	}
+	return h.slice
+}
